785089782849f4ea6777bfc0013aad5605bcb748
Israq <35467653+Israq@users.noreply.github.com>
2018-11-07 09:20:42 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<< "Enter the first number \n";
+    cin>>a;
+    cout<<"enter the second number \n";
+    cin>>b;
+    if(a==b){
+    cout<<"your test is successful";
+
+
+    }else{
+
+    cout<<"your test is not successful";
+    }
+    return 0;
+}
